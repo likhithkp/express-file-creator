ad1a7a9495b0c1203b7fd4b60fe75704895b8f0f
likhithkp <likhithkpdev@gmail.com>
2024-11-09 23:55:22 +0530

    Get and post file

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const fs = require("fs");
+const { randomFill } = require("crypto");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+const fileDir = path.join(__dirname, "files");
+if(!fs.existsSync(fileDir)){
+    fs.mkdirSync(fileDir);
+};
+
+app.get("/", (req, res) => {
+    fs.readdir(fileDir, (err, files) => {
+        if(err){
+            console.log(err);
+            return;
+        };
+        res.render("index", {files: files});
+    });
+});
+
+app.post("/create", (req, res) => {
+    const fileName = req.body.title.split(" ").join("") + ".txt";
+    fs.writeFile(`./files/${fileName}`, req.body.details, (err) => {
+        if(err){
+            console.log(err);
+            return;
+        };
+        console.log("File created")
+        res.redirect("/");
+    });
+});
+
+app.listen(3001, () => {
+    console.log("Server listening...");
+});
